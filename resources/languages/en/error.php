f35d0f54dc25a6bd17bcb35e67fb6229ff883dbc
selcukcukur <selcukcukur@outlook.com.tr>
2025-10-20 17:08:49 +0300

    initial error page scaffolding

---
+++ resources/languages/en/error.php
@@ -0,0 +1,211 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 401
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '401'           => [
+        'code'          => '401',
+        'title'         => 'Yetkisiz Erişim',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Erişim sağlamaya çalıştığınız kullanıcı adı veya parola hatalı olabilir.',
+            'Erişim sağlamaya çalıştığınız anahtar hatalı veya geçersiz olabilir',
+            'Erişim sağlamaya çalıştığınız servis artık devre dışı bırakılmış olabilir.',
+            'Erişim sağlamaya çalıştığınız bilgiler otomatik olarak engellenmiş olabilir.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 402
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '402'           => [
+        'code'          => '402',
+        'title'         => 'Ödeme Gerekli',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Erişim sağladığınız sayfayı görüntülemek için ödeme yapmamış olabilirsiniz.',
+            'Erişim sağladığınız sayfanın yapılandırması ile ilgili bir problem olabilir.',
+            'Erişim sağladığınız sayfa artık ücretli abonelik ile sunuluyor olabilir.',
+            'Erişim sağladığınız sayfaya erişmek için yetkiniz veya aboneliğiniz olmayabilir.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 403
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '403'           => [
+        'code'          => '403',
+        'title'         => 'Yetkisiz Erişim',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Erişim izniniz olmayan bir dizine erişim sağlamaya çalışıyor olabilirsiniz.',
+            'Erişim sağladığınız dizin ile ilgili bakım ve güncelleme çalışması olabilir.',
+            'Erişim sağladığınız sayfa ziyaretçilere uygun bir sayfa olmayabilir.',
+            'Erişim sağlamak istediğiniz sayfanın bağlantı adresi hatalı olabilir.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 404
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '404'           => [
+        'code'          => '404',
+        'title'         => 'Sayfa bulunamadı',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Görüntülemek istediğiniz sayfa artık mevcut olmayabilir.',
+            'Görüntülemek istediğiniz bağlantı geçersiz olabilir.',
+            'Görüntülemek istediğiniz sayfanın adresi değişmiş olabilir.',
+            'Görüntülemek istediğiniz sayfanın url adresi hatalı olabilir.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 419
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '419'           => [
+        'code'          => '419',
+        'title'         => 'Zaman Aşımı',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Çok uzun süre işlem yapmadıktan sonra sayfaya erişim sağlıyor olabilirsiniz.',
+            'Erişim sağladığınız sayfa direkt olarak yapılan istekleri kabul etmiyor olabilir.',
+            'Erişim sağlamak için kullandığınız yöntem hatalı yapılandırılmış olabilir.',
+            'Arka arkaya sayfayı yenileyerek erişim sağlamaya çalışıyor olabilirsiniz.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 429
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '429'           => [
+        'code'          => '429',
+        'title'         => 'Çok fazla istek',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Erişim sağlamak istediğiniz izin verilen istek limitine ulaşmış olabilirsiniz.',
+            'Erişim sağlamak istediğiniz sayfanın yapılandırmasında bir hata olabilir.',
+            'Sunucu, yazılım veya yetkililerin belirlediği limiti aşmış olabilirsiniz.',
+            'Erişim sağlamak istediğiniz sayfanın url adresi hatalı olabilir.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 500
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '500'           => [
+        'code'          => '500',
+        'title'         => 'Sunucu hatası',
+        'mascot'        => [
+            'title'         => 'Hay aksi, bir şeyler yolunda gitmiyor!',
+            'description'   => 'Talebinizi yerine getirmeye devam ederken beklenmeyen bir problem ile karşılaştık bunun başlıca sebepleri kullanıcı taraflı hatalar olabilir eğer kullanıcı taraflı bir hata olmadığını düşünüyorsanız bizim ile iletişime geçebilir ve yaşadığınız sorunu bize bildirebilirsiniz.',
+        ],
+        'description'   => 'Erişim sağlamak istediğiniz sayfa ile ilgili işlemleri sürdürürken bir problem ile karşılaştık bunun birden çok nedeni olabilir size yardımcı olabilecek bazı kısa ipuçlarını sizin için derledik.',
+        'tips'          => [
+            'Sunucu bakım, güncelleme veya taşıma işlemleri yapılıyor olabilir.',
+            'Veri merkezi veya internet erişim ağı ile ilgili bir problem olabilir.',
+            'Yazılımın hatalı bir şekilde yapılandırılması ile ilgili bir sorun olabilir.',
+            'Sunucu yapılandırma ayarlarının hatalı yapılması ile ilgili bir sorun olabilir.',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Error : 503
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    |
+    */
+    '503'           => [
+        'code'          => '503',
+        'title'         => 'Yenileniyoruz',
+        'mascot'        => [
+            'title'         => 'Yakında döneceğiz, şu an yenileniyoruz!',
+            'description'   => 'Değerli kullancılarımızın hizmet ve deneyimini en yüksek seviyeye taşımak adına şuan da bakım, onarım ve yenileme çalışmaları devam etmektedir, tahmini planlanan bakım süresi bilinmemektedir, sabrınız ve anlayışınız için teşekkürler en kısa sürede görüşmek üzere!',
+        ],
+        'description'   => 'Sistem bakım, onarım ve iyileştirme çalışmaları devam etmektedir kısa süre içerisinde sizlere tekrardan hizmet vermeye başlayacağız sabrınız ve anlayışınız için teşekkürler, görüşmek üzere!',
+        'tips'          => [
+            'Ürün, hizmet ve servislerimize yeni ayrıcalıklar ekliyoruz.',
+            'Güvenlik güncellemeleri ve yeni teknolojileri uyguluyoruz.',
+            'Servis ve API hizmetlerimizi güncelliyor güvende tutuyoruz.',
+            'Tasarım ve kullanıcı arayüzü iyileştirmelerini uyguluyoruz.',
+        ],
+    ]
+];
--- src/LarawiseProvider.php
+++ src/LarawiseProvider.php
@@ -2,15 +2,18 @@
 
 namespace Larawise;
 
+use Illuminate\View\Factory;
+use Larawise\Console\CacheCommand;
 use Larawise\Packagify\Packagify;
 use Larawise\Packagify\PackagifyProvider;
+use Larawise\Service\CacheService;
 
 /**
  * Srylius - The ultimate symphony for technology architecture!
  *
  * @package     Larawise
  * @subpackage  Core
- * @version     v0.0.1
+ * @version     v1.0.0
  * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
  * @copyright   Srylius Teknoloji Limited Şirketi
  *
@@ -27,7 +30,32 @@ class LarawiseProvider extends PackagifyProvider
      */
     public function configure(Packagify $package)
     {
-        $package->name('larawise')
-            ->description('Larawise - The most powerful, elegant, and productive way to develop your next Laravel application.');
+        // Configure the package name.
+        $package->name('larawise');
+
+        // Configure the package description.
+        $package->description('Larawise - The most powerful, elegant, and productive way to develop your next Laravel application.');
+
+        $package->hasConfigurations();
+
+        $package->hasTranslations();
+
+        $package->hasSingletons([
+            'larawise.cache' => fn ($app) => new CacheService($app['cache'], $app['config']['larawise.cache.service'])
+        ]);
+
+        // Configure the package commands.
+        $package->hasCommands([
+            CacheCommand::class
+        ]);
+    }
+
+    public function packageRegistering()
+    {
+        // What needs to be done before the application's view factory is resolved.
+        $this->app->afterResolving('view', function (Factory $levd) {
+            // Include levd extension in blade view engine.
+            $levd->addExtension('levd', 'blade');
+        });
     }
 }
